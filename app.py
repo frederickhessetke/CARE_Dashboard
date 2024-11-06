dabdb0d480f54aa80ea8a42fc4ce3d08ed8402fa
frederickhessetke <frederick.hesse@tkelevator.com>
2024-11-06 12:42:47 -0500

    Update app.py
    DB_PATH update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from datetime import datetime, timedelta
 
 # Updated database path with new Routes table
-DB_PATH = r'C:\Users\HESSEFREDERICK\PycharmProjects\CARE_Dashboard\data\CARE_Database.db'
+DB_PATH = 'data/CARE_Database.db'
 
 # Connect to database
 def connect_db():
